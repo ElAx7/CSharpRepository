edc9032f5a66510108ee79e3ddd7d1929f643e8b
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2024-08-16 13:10:01 -0600

    Strings

---
+++ Solution/CSharpPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CSharpPractice.Math;
+
+namespace Variables
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var axel = new Person();
+            axel.FirstName = "Axel";
+            axel.LastName = "Orozco";
+            axel.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Axel";
+            var lastName = "Orozco";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0}{1}", firstName, lastName);
+
+            var names = new string[3] { "axel", "juan", "cielo" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi Axel
+Look into the following paths
+c:\folder1\folder2";
+            Console.WriteLine(text);
+        }
+    }
+}
