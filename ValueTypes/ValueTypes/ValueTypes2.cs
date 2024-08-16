ad689da5d21e4ba21e7dedc001d86aa6e5509e03
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2024-08-16 13:11:10 -0600

    Enums, Lists and ValueTypes

---
+++ ValueTypes/ValueTypes/ValueTypes2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValueTypes
+{
+	public class Person
+	{
+		public int Age;
+	}
+
+	class ValueTypes
+
+	{
+		static void Main(string[] args)
+		{
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+		}
+
+        //Method 1 no regresan ningun valor
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        //Method 2 no regresan ningun valor
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
+
