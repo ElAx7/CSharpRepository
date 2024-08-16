9861d01f69f0fb819edde23009ef3c2d6c251dcb
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2024-08-16 16:18:45 -0600

    File, FileClasses, Directory, DirectoryClasses & PATH

---
+++ FileClasses/FileClasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace FileClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\something.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+
+            //------------------------------------------------------------//
+
+            //Directory & DirectoryInfoClasses
+
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\FileClasses", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(files);
+        }
+    }
+}
+
