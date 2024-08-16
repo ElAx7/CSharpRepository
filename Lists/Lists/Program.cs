84de32154d7b351790b16e59a408cb654b45160c
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2024-08-16 13:48:23 -0600

    Lists

--- Lists/Lists/Program.cs
+++ Lists/Lists/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
 
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] {5 , 6, 7});
+
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine();
+            Console.WriteLine("Index of 1:" + numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1:" + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+        }
+    }
+}
